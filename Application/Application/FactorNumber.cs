4123aac8b0f669481728d3dff6b9b922c7dd4e21
hardimajmundar <hardimajmundar638@gmail.com>
2018-05-16 13:22:32 -0500

    Initial commit.

---
+++ Application/Application/FactorNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    class FactorNumber
+    {
+        static void Main(string[] args)
+        {
+            int factornumber = 0;
+            Console.WriteLine("Enter the Number", +factornumber);
+            Console.ReadLine();
+
+            if (factornumber % 3 == 0)
+            {
+                Console.WriteLine("Pling");
+            }
+            else if (factornumber % 5 == 0)
+            {
+                Console.WriteLine("Plang");
+            }
+            else if (factornumber % 7 == 0)
+            {
+                Console.WriteLine("Plong");
+            }
+            else
+            {
+                Console.WriteLine("Number" + factornumber);
+            }
+            Console.ReadKey();
+        }
+    }
+}
